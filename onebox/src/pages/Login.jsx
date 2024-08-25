0675a00aa9b5ccc7ac59a85b561c7781e6b46c3e
unknown <vardhan70139@gnail.com>
2024-08-25 11:37:37 +0530

    deploy redirect link change

--- onebox/src/pages/Login.jsx
+++ onebox/src/pages/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
                     <p  className=' text-xl mb-6  ml-24'>Create new account</p>
                     <div className='rounded border border-gray-500 h-12 flex justify-center items-center gap-2.5  mb-12'>
                         <img src="https://static.vecteezy.com/system/resources/previews/013/760/951/non_2x/colourful-google-logo-in-dark-background-free-vector.jpg" alt=""  className='bg-black rounded-full h-5 mt-0.5'/>
-                        <Link  to="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:5173/home" className='text-sm text-gray-400 cursor-pointer'> Sign Up with Google </Link>
+                        <Link  to="https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://one-box-ebon.vercel.app/home" className='text-sm text-gray-400 cursor-pointer'> Sign Up with Google </Link>
                     </div>
                     <p className='w-[195px] h-12 bg-[#3d65ea] rounded m-auto text-center pt-2.5 mb-6 cursor-pointer'>Create an account</p>
                     <p className='text-base text-gray-500 ml-20'>Already have an account? <span style={{cursor:'pointer'}}>Sign In</span></p>
